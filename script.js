271ad475813b6729532858ee77b3f61b2daaea01
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-02-27 02:16:52 -0600

    finished if statments

--- script.js
+++ script.js
@@ -28,17 +28,92 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
  passwordlength = window.prompt("Choose a password lenght between 1 and 128")
  if (passwordlength < 8 || passwordlength >128) {
-   var passwordlength = window.alert ("Incorrect length"+ passwordlength);   
+   var passwordlength = window.log ("Incorrect length" + passwordlength);   
  }
-  else if (passwordlength >= 8 || passwordlength <=128)
+  else if (passwordlength >= 8 || passwordlength <=128)  
   var special = confirm ("Would you like to use a Special character?");
+  console.log("Special" + special)
   var lower = confirm ("Would like to use a lowercase character?");
+  console.log("Lower" + lower)
   var upper = confirm ("Would like to use an Uppercase character?");
+  console.log("upper" + upper)
   var number = confirm ("Would you like to use Numbers?")
+  console.log("number" + number)
 
+};
+// all 4 preferences
   if (special && lower && upper && number) {
-    
+    preferences = special.concat(lower, upper, number);
+    console.log(preferences); 
   }
+//  3 prefences 
+else if (special && lower && upper && number) {
+  preferences = special.concat(lower, upper);
+  console.log(preferences);
+} 
+
+else if (special && lower && upper && number) {
+    preferences = special.concat(lower, number);
+    console.log(preferences);
+}
+
+else if (special && lower && upper && number) {
+  preferences = special.concat(upper, number);
+  console.log(preferences);
+}
+
+else if (special && lower && upper && number) {
+  preferences = upper.concat(lower, number);
+  console.log(preferences);
+} 
+
+  // 2 preferences
+  else if (special && lower && upper && number) {
+    preferences = special.concat(lower);
+    console.log(preferences); 
+  }
+
+  else if (special && lower && upper && number) {
+      preferences = special.concat(upper);
+      console.log(preferences);
+  }
+  
+  else if (special && lower && upper && number) {
+    preferences = special.concat(number);
+    console.log(preferences);
+  }
+  
+  else if (special && lower && upper && number) {
+    preferences = lower.concat(upper);
+    console.log(preferences);
+  }
+  
+  else if (special && lower && upper && number) {
+    preferences = lower.concat(number);
+    console.log(preferences);
+  }
+  
+  //1 preferences
+  else if (special && lower && upper && number) {
+    preferences = special;
+    console.log(preferences);
+  }  
+    else if (special && lower && upper && number) {
+      preferences = upper;
+      console.log(preferences);
+  } 
+
+  else if (special && lower && upper && number) {
+    preferences = number;
+    console.log(preferences);
+  }  
+
+  else if (special && lower && upper && number) {
+    preferences = lower;
+    console.log(preferences);
+  } 
+
+
 
  
   var password = generatePassword();
@@ -46,7 +121,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
