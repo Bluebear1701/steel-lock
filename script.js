beba94ccfef0a0178734ac9f4811ac7ffe298b46
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-02-26 21:38:13 -0600

    added random number and mathfloor mathrandom

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 // Assignment code here
-// Assignment code here
 //validate password function 
+var passwordlength
+var confirmspecial 
+var confirmlower
+var confirmupper
+var confirmnumber
 //var special characters
-var specialchar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", " < ", "=", " > ", " ? ", "@", " ^ ", "_", "~"];
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", " < ", "=", " > ", " ? ", "@", " ^ ", "_", "~"];
 //var lower case
-var lowerchar = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lower = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //var upper case
-var upperchar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 //var number
 var number = ["1", "2", "3", "4", "5", "6", "7", "8","9","0"];
-//mathrandom
-// need a comfirm for each var
+
+//function to generate a random numeric value
+var randomNumber = function () {
+var value = Math.floor(Math.random() * 10) + 128
+
+return value;
+};
+// need a comfirm for each var if, if else
 // step 1 validate number between 1 -128
 // need prompt to confirm 
 
@@ -19,6 +29,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -29,18 +40,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
